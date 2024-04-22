7e3861108cc329f825087d1801c9fb756161e3f7
Makosai <kristopher.ali.dev@gmail.com>
2024-04-21 22:50:46 -0400

    Write tests for the Engine.

---
+++ engine_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"testing"
+)
+
+// NewClient should create a local client with an ID of 1.
+func TestLocalClient(t *testing.T) {
+	client := NewClient()
+	if client.id == 1 {
+		t.Log("Client ID is 1")
+	} else {
+		t.Error("Client ID is not 1")
+	}
+}
+
+// NewClientRemote should set the ID properly.
+func TestRemoteClient(t *testing.T) {
+	client := NewClientRemote(2)
+	if client.id == 2 {
+		t.Log("Client ID is 2")
+	} else {
+		t.Error("Client ID is not 2")
+	}
+
+	client.id
+}
+
+// NewClientRemote should panic if the ID is 1.
+func TestRemoteClientPanic(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The code did not panic")
+		} else {
+			t.Log("The code panicked")
+		}
+	}()
+	NewClientRemote(1)
+}
